6843f0869484c5e4b563d94a3f7932e7470034d6
Robik <>
2012-06-22 08:43:52 +0545

    First commit my dear

---
+++ TryGit_001/src/trygit_001/TryGit_001.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trygit_001;
+
+/**
+ *
+ * @author Dell
+ */
+public class TryGit_001 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!");
+    }
+}
